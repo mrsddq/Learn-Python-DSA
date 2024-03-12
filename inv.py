4cb0ad376be4c474045107df65f30425d94f120a
Laraib <laraibks@gmail.com>
2024-03-12 21:34:26 +0530

    Selenium

--- inv.py
+++ inv.py
@@ -1,19 +1,6 @@
-# def fact(n):
-#     if n == 0 or n == 1:
-#         return 1
-#     else:
-#         return n*fact(n-1)
-
-# n=int(input())
-# f=fact(n)
-# print(f)
-
-def print1ToN(n):
-    if n==0:
-        return
-    print1ToN(n-1)
-    print(n)
-    return
-n=int(input())
-p=print1ToN(n)
-print(p)
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from time import sleep
+driver = webdriver.Chrome()
+driver.get('https://www.rapidtables.com/tools/click-counter.html')
